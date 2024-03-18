a8bc0f345650adff0bdf517689e39335b5f2f57e
Daniel Escobar <danielescobar.987@gmail.com>
2024-03-17 19:16:27 -0600

    {ADD} Initial view

--- App.tsx
+++ App.tsx
@@ -1,118 +1,80 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { SafeAreaView, Text, StyleSheet, Pressable, View, Image } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Title = () => (
+  <Text style={ styles.title }>
+    Doctor { '' }
+    <Text style={ styles.title_bold }>Link</Text>
+  </Text>
+);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = (): React.JSX.Element => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={ styles.container }>
+      <View style={ styles.content }>
+        <View style={ styles.imageContainer }>
+          <Image
+            source={ require( './src/assets/img/doctor-link.jpg' ) }
+            style={ styles.image }
+          />
         </View>
-      </ScrollView>
+        <Title />
+        <Text style={ styles.subtitle }>Encuentra a los mejores médicos.</Text>
+        <Pressable style={ styles.button }>
+          <Text style={ styles.buttonText }>Iniciar</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const styles = StyleSheet.create( {
+  container: {
+    backgroundColor: '#F3F4F6',
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 50,
+    color: '#36CFC9',
     fontWeight: '600',
   },
-  sectionDescription: {
-    marginTop: 8,
+  title_bold: {
+    fontWeight: '900',
+    color: '#6D28D9',
+  },
+  subtitle: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#374151',
+  },
+  button: {
+    backgroundColor: '#36CFC9',
+    padding: 15,
+    marginTop: 365,
+    marginLeft: 20,
+    marginRight: 20,
+    borderRadius: 40,
+  },
+  buttonText: {
+    color: '#fff',
     fontSize: 18,
-    fontWeight: '400',
+    textAlign: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+} );
 
 export default App;
